b59ef6e39f8bbf94426d9cd19dbc7c2b6df7efc2
Tonyweng <xinweng47@gmail.com>
2026-04-04 01:44:46 +0000

    terraform complete

--- Terrademo/main.tf
+++ Terrademo/main.tf
@@ -8,14 +8,15 @@ terraform {
 }
 
 provider "google" {
-    project     = "data-engineering-492202"
-    region      = "us-central1"
+  credentials = file(var.credentials)
+  project = var.project
+  region  = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "data-engineering-492202-terra-bucket"
-  location      = "US"
-  force_destroy = true
+  name                        = var.gcs_bucket_name
+  location                    = var.location
+  force_destroy               = true
   uniform_bucket_level_access = true
 
   lifecycle_rule {
@@ -26,4 +27,10 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location = var.location
 }
