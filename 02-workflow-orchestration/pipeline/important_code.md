12832d40d392d719b8c4aba2093c1794a6dae1d1
Tonyweng <xinweng47@gmail.com>
2026-04-08 02:37:53 +0000

    Kestra end 2.3.2

---
+++ 02-workflow-orchestration/pipeline/important_code.md
@@ -0,0 +1,12 @@
+docker network create pg-network
+
+docker-compose up -d
+
+docker-compose down
+
+**Kestra**
+username: "admin@kestra.io" 
+password: Admin1234!
+
+PGADMIN_DEFAULT_EMAIL=admin@admin.com
+PGADMIN_DEFAULT_PASSWORD=root
---
+++ 02-workflow-orchestration/pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+
+import pandas as pd
+
+print("arguments", sys.argv)
+month = int(sys.argv[1])
+print(f"Running pipeline for day {month}")
+
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_day_{month}.parquet")
